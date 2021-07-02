c4d7e618c48dce0c3333f4a8a467281212b6c5b8
Luiz Eduardo <programadorlesd@gmail.com>
2021-07-01 23:32:51 -0300

    Developed person creat operation

--- src/main/java/one/digitalinnovation/PersonapiApplication.java
+++ src/main/java/one/digitalinnovation/PersonapiApplication.java
@@ -1,4 +1,4 @@
-package one.digitalinnovation.personapi;
+package one.digitalinnovation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
